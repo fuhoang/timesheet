8f39ad11058b67171a788da6336c2f168f5f4947
Fu Hoang <fuhoang84@googlemail.com>
2026-02-03 21:33:49 +0000

    Stabilize tests and fix admin flag

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,20 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Contracts\Console\Kernel;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication()
+    {
+        $envPath = dirname(__DIR__) . '/.env';
+        if (!file_exists($envPath)) {
+            file_put_contents($envPath, "APP_KEY=base64:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=\n");
+        }
+
+        $app = require __DIR__.'/../bootstrap/app.php';
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
